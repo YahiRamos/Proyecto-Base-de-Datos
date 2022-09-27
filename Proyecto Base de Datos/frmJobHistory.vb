5b1586b5bfe48e362eb002fd8161272e23f9b87f
YAHIR RAMOS <yahiramoslinks@hotmail.com>
2022-09-27 01:28:02 -0500

    creacion de form job history (design y funciones basicas)

--- Proyecto Base de Datos/frmJobHistory.vb
+++ Proyecto Base de Datos/frmJobHistory.vb
@@ -1,3 +1,18 @@
 ﻿Public Class frmJobHistory
+    Private Sub btnBackMenu_Click(sender As Object, e As EventArgs) Handles btnBackMenu.Click
+        Close()
+        menuPrincipal.Visible = True
+    End Sub
 
+    Private Sub frmJobHistory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnEliminar.Visible = False
+        btnModificar.Visible = False
+        btnRegistrar.Visible = False
+        btnVerDatos.Visible = False
+        txtDepartmentId.Enabled = False
+        txtEmployeeId.Enabled = False
+        txtEndDate.Enabled = False
+        txtJobId.Enabled = False
+        txtStartDate.Enabled = False
+    End Sub
 End Class
