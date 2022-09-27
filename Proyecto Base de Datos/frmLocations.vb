cf4dfb88f1c78d423e2047e46131e89400a85ade
YAHIR RAMOS <yahiramoslinks@hotmail.com>
2022-09-27 01:13:20 -0500

    creacion de form locations (design y funciones basicas)

--- Proyecto Base de Datos/frmLocations.vb
+++ Proyecto Base de Datos/frmLocations.vb
@@ -1,3 +1,19 @@
 ﻿Public Class frmLocations
+    Private Sub btnBackMenu_Click(sender As Object, e As EventArgs) Handles btnBackMenu.Click
+        Close()
+        menuPrincipal.Visible = True
+    End Sub
 
+    Private Sub frmLocations_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnEliminar.Visible = False
+        btnModificar.Visible = False
+        btnRegistrar.Visible = False
+        btnVerDatos.Visible = False
+        txtCity.Enabled = False
+        txtCountryId.Enabled = False
+        txtLocationId.Enabled = False
+        txtPostalCode.Enabled = False
+        txtStateProvince.Enabled = False
+        txtStreetAddress.Enabled = False
+    End Sub
 End Class
